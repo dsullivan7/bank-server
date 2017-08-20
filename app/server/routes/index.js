397031fba3a1cbac2cf17fbf3ca81e2379b6f460
dsullivan7 <dbsullivan23@gmail.com>
2017-08-20 19:45:36 +1000

    adding a bit more documentation

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -22,6 +22,7 @@ apiRoutes.get('/', (req, res) => res.status(200).send({
 
 /**
  * @api {post} /users Create User
+ * @apiVersion 0.1.0
  * @apiGroup Users
  * @apiName CreateUser
  *
@@ -47,6 +48,7 @@ apiRoutes.post('/users', usersController.create)
 
 /**
  * @api {get} /users List Users
+ * @apiVersion 0.1.0
  * @apiGroup Users
  * @apiName ListUsers
  *
@@ -77,6 +79,7 @@ apiRoutes.get('/users', usersController.list)
 
 /**
  * @api {get} /users/:id Get User
+ * @apiVersion 0.1.0
  * @apiGroup Users
  * @apiName GetUser
  *
@@ -108,6 +111,7 @@ apiRoutes.get('/users/:userId', usersController.retrieve)
 
 /**
  * @api {put} /users/:id Modify User
+ * @apiVersion 0.1.0
  * @apiGroup Users
  * @apiName ModifyUser
  *
@@ -142,6 +146,7 @@ apiRoutes.put('/users/:userId', usersController.update)
 
 /**
  * @api {delete} /users/:id Delete User
+ * @apiVersion 0.1.0
  * @apiGroup Users
  * @apiName DeleteUser
  *
@@ -160,10 +165,63 @@ apiRoutes.put('/users/:userId', usersController.update)
  */
 apiRoutes.delete('/users/:userId', usersController.destroy)
 
+/**
+ * @api {post} /accounts/:id Create Account
+ * @apiVersion 0.1.0
+ * @apiGroup Accounts
+ * @apiName CreateAccount
+ *
+ * @apiParam (Path Parameters) {Number} id Account's unique ID
+ *
+ * @apiParam (Body Parameters) {String} [currency]  Optional the new currency of the Account
+ * @apiParam (Body Parameters) {Number} [balance]   Optional new Balance of the Account
+ * @apiParam (Body Parameters) {Object[]} [Users]   Array of User Ids to associate with the account
+ *
+ * @apiSuccess (Response Fields) {String} currency Currency of the Account
+ * @apiSuccess (Response Fields) {Number} balance  Balance of the Account
+ * @apiSuccess (Response Fields) {Object[]} Users  Array of Users associated with the Account
+ * @apiSuccess (Response Fields) {Number} id  Id of the Account
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "currency": "USD",
+ *       "balance": 100.99,
+ *       "Users": [1, 2]
+ *     }
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "currency": "USD",
+ *       "balance": 100.99,
+ *       "Users": [
+ *       {
+ *         "firstname": "Michael",
+ *         "lastname": "Jordan",
+ *         "id": 1
+ *       },
+ *       {
+ *         "firstname": "Kobe",
+ *         "lastname": "Bryant",
+ *         "id": 2
+ *       }
+ *     ],
+ *       "id": 1
+ *     }
+ *
+ * @apiError AccountNotFound The id of the Account was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "Account Not Found"
+ *     }
+ */
 apiRoutes.post('/accounts', accountsController.create)
 
 /**
  * @api {get} /accounts List Accounts
+ * @apiVersion 0.1.0
  * @apiGroup Accounts
  * @apiName ListAccounts
  *
@@ -193,15 +251,70 @@ apiRoutes.get('/accounts', accountsController.list)
 
 /**
  * @api {get} /accounts/:id Get Account
+ * @apiVersion 0.1.0
  * @apiGroup Accounts
  * @apiName GetAccount
  *
  * @apiParam (Path Parameters) {Number} id Account's unique ID
  *
- * @apiSuccess (Response Fields) {String} firstname Firstname of the Account
- * @apiSuccess (Response Fields) {String} lastname  Lastname of the Account
- * @apiSuccess (Response Fields) {Object[]} Accounts  Array of Users associated with the Account
+ * @apiSuccess (Response Fields) {String} currency Currency of the Account
+ * @apiSuccess (Response Fields) {Number} balance  Balance of the Account
+ * @apiSuccess (Response Fields) {Object[]} Users  Array of Users associated with the Account
  * @apiSuccess (Response Fields) {Number} id  Id of the Account
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "currency": "USD",
+ *       "balance": 100.99,
+ *       "Users": [
+ *       {
+ *         "firstname": "Kobe",
+ *         "lastname": "Bryant",
+ *         "id": 2
+ *       }
+ *       ],
+ *       "id": 1
+ *     }
+ *
+ * @apiError AccountNotFound The id of the Account was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "message": "Account Not Found"
+ *     }
+ */
+apiRoutes.get('/accounts/:accountId', accountsController.retrieve)
+
+/**
+ * @api {put} /accounts/:id Modify Account
+ * @apiVersion 0.1.0
+ * @apiGroup Accounts
+ * @apiName ModifyAccount
+ *
+ * @apiParam (Path Parameters) {Number} id Account's unique ID
+ *
+ * @apiParam (Body Parameters) {String} [currency]  Optional the new currency of the Account
+ * @apiParam (Body Parameters) {Number} [balance]  Optional new Balance of the Account
+ * @apiParam (Body Parameters) {Object} [Users]  Users modification object
+ * @apiParam (Body Parameters) {Object[]} [Users.add]  User Ids to add or remove to the account
+ * @apiParam (Body Parameters) {Object[]} [Users.remove]  User Ids to remove to the account
+ *
+ * @apiSuccess (Response Fields) {String} currency Currency of the Account
+ * @apiSuccess (Response Fields) {Number} balance  Balance of the Account
+ * @apiSuccess (Response Fields) {Object[]} Users  Array of Users associated with the Account
+ * @apiSuccess (Response Fields) {Number} id  Id of the Account
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "currency": "USD",
+ *       "balance": 100.99,
+ *       "Users": {
+ *         "add": [1, 2],
+ *         "remove": [3],
+ *       },
+ *     }
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
@@ -220,12 +333,11 @@ apiRoutes.get('/accounts', accountsController.list)
  *       "message": "Account Not Found"
  *     }
  */
-apiRoutes.get('/accounts/:accountId', accountsController.retrieve)
-
 apiRoutes.put('/accounts/:accountId', accountsController.update)
 
 /**
  * @api {delete} /accounts/:id Delete Account
+ * @apiVersion 0.1.0
  * @apiGroup Accounts
  * @apiName DeleteAccount
  *
