c5af8bda6ec6a448a2a90f7329546ac483257d96
dsullivan7 <dbsullivan23@gmail.com>
2017-08-20 20:46:50 +1000

    removing host setting

--- app/bin/start.js
+++ app/bin/start.js
@@ -2,11 +2,10 @@ import http from 'http'
 import app from '../app'
 
 const port = process.env.PORT || 8000
-const host = process.env.HOST || 'localhost'
 
 app.set('port', port)
 
 const server = http.createServer(app)
-server.listen(port, host)
+server.listen(port)
 
-console.log(`Bank Server has started on http://${host}:${port}`)
+console.log(`Bank Server has started on port ${port}`)
